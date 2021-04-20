7c9277767506ebfbf9d8dfbd810a0789fd236efc
youbyoub48 <ayou8@hotmail.fr>
2021-04-20 15:15:34 +0200

    Test API

--- API.py
+++ API.py
@@ -1,7 +1,14 @@
 import os
 boucle = True
+PATH = os.path.abspath(os.path.split(__file__)[0])
+print(PATH)
 while boucle:
     command = input("#")
     if command == "exit":
         boucle = False
-    print(os.system(command))
+    if command.count("cd") == 1:
+        PATH = command.replace("cd ","")
+    command = "cd "+PATH+"&&"+command
+    a = os.system(command)
+    a = str(a)
+    print(a.replace("0",""))
